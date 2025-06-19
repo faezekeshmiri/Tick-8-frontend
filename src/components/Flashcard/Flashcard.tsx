4a576be10d65e8627f4458b3c913a7cbdbfa6ac4
Faezeh <keshmirifaezeh@gmail.com>
2025-06-19 12:56:44 +0200

    Create Basic Component.

--- src/components/Flashcard/Flashcard.tsx
+++ src/components/Flashcard/Flashcard.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 interface FlashcardProps {
-  name: string;
+  word: string;
+  type: string;
+  pronunciation: string;
+  translation: string;
+  example: string;
 }
 
-const Flashcard: React.FC<FlashcardProps> = ({ name }) => {
+const Flashcard: React.FC<FlashcardProps> = ({ word, type, pronunciation, translation, example }) => {
   return (
     <div>
-      <h1>Hello, {name}!</h1>
+      <Card variant="outlined" sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <h2>{word}</h2>
+          <h5>{type}</h5>
+          <h5>{pronunciation}</h5>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button } from "@mui/material"
+import Flashcard from "../components/Flashcard";
 
 const Home = () => {
   return (
     <>
       <h2 className="text-2xl font-bold underline">Home Page</h2>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
+      
+      <Flashcard word="Good" type="adjective" pronunciation="good" translation="salam" example="The weather looks good today."/>
     </>
     );
   };
